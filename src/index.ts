25a8e9a79d3fdba64db27a47727afc2a2baac383
MoeinDanA <moein.danesh97@gmail.com>
2024-10-19 11:30:29 +0330

    server update

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import webpush, { PushSubscription } from "web-push";
 import { cors } from "@elysiajs/cors";
 
@@ -8,24 +8,51 @@ webpush.setVapidDetails(
   process.env.vapid_private_key ?? ""
 );
 
+let sub: PushSubscription | undefined = undefined;
+
 const app = new Elysia()
   .use(cors())
   .get("/", () => "Hello Elysia")
-  .post("/subscribe", ({ body }) => {
-    console.log(body);
-    const subscription = body as PushSubscription;
-    const payload = JSON.stringify({
-      title: "Hello World",
-      body: "This is your first push notification",
-    });
-    webpush
-      .sendNotification(subscription, payload)
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => {
-        console.log(err);
+  .post(
+    "/subscribe",
+    ({ body }) => {
+      console.log(body);
+      const subscription = body.subscription as PushSubscription;
+      sub = subscription;
+      const payload = JSON.stringify({
+        title: "Hello World",
+        body: "This is your first push notification",
       });
+      webpush
+        .sendNotification(subscription, payload)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    {
+      body: t.Object({
+        subscription: t.Any(),
+        userId: t.String(),
+        device: t.Object({
+          type: t.String(),
+          model: t.String(),
+          browser: t.String(),
+          browserVersion: t.String(),
+        }),
+      }),
+    }
+  )
+  .get("/send", () => {
+    if (sub) {
+      const payload = JSON.stringify({
+        title: "Hello World",
+        body: "This is another push notification",
+      });
+      webpush.sendNotification(sub, payload);
+    }
   })
   .listen(3002);
 
